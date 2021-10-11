00c4dc4fc54827d00b6712de0e3a4a2bb2b48abd
jun watanabe windows <junwatanabe72@gmail.com>
2021-10-11 15:44:05 +0900

    re

--- index.js
+++ index.js
@@ -1,58 +1,140 @@
+// (start)baseを選択する>四則演算を選ぶ(middle)>subを選ぶ(end)>四則演算か=を選択する(middle or start)
+
+// variale
 const calPhase = {
   start: 0,
   middle: 1,
   end: 2,
 };
-
-const globalNumber = {
+const initalNumber = {
   base: "",
   sub: "",
   ari: "",
   phase: calPhase.start,
 };
 
-const Arithmetic = {
-  sum: "+",
-  sub: "-",
-  mult: "*",
-  divi: "%",
+let globalNumber = {
+  ...initalNumber,
 };
 
-//数値の要素（.）含むdom取得
-const buttonsElement = document.querySelectorAll(".js-button");
-//Arithmeticのdom取得
-const actionButtonsElement = document.querySelectorAll(".js-action-button");
-//合計値表示dom
-const totalPointElement = document.querySelector(".total-point");
-//初期化するdom
-const resetButtonElement = document.querySelector(".js-reset-button");
-//出力するdom
-const outputButtonElement = document.querySelector(".js-output-button");
-
-//リセットアクション
-resetButtonElement.addEventListener("click", resetArray);
-function resetArray() {
-  globalNumber.base = 0;
-  globalNumber.sub = "";
-  globalNumber.ari = "";
-  globalNumber.phase = calPhase.start;
-  globalNumber.base = "";
+// function
+const resetArray = () => {
+  globalNumber = { ...initalNumber };
   totalPointElement.innerHTML = 0;
-}
+};
 
-//計算するベースの数値を作る
-buttonsElement.forEach((item) => {
-  item.addEventListener("click", function () {
-    createNumber(item);
-  });
-});
-//baseとsubの数値を定義する。
-function createNumber(e) {
-  //ここから、base,sub共通の例外処理
-  //base 最初に "." を押された場合の処理
+const isdecimalPoint = (e, stringNum) => {
+  return !stringNum.includes(e);
+};
+
+const adjustedNumberOfDigits = (n) => {
+  const num = 16;
+  let nString = String(n);
+  if (nString.length > num) {
+    nString = nString.slice(nString.length - num);
+    console.log(nString);
+  }
+  return Number(nString);
+};
+
+const processNumber = (cir) => {
+  globalNumber = {
+    ...globalNumber,
+    base: Number(globalNumber.base),
+    sub: Number(globalNumber.sub),
+  };
+
+  const arithmetic = {
+    sum: globalNumber.base + globalNumber.sub,
+    sub: globalNumber.base - globalNumber.sub,
+    mult: adjustedNumberOfDigits(globalNumber.base * globalNumber.sub),
+    div: globalNumber.base / globalNumber.sub,
+  };
+
+  return arithmetic[cir];
+};
+
+const totalNumber = () => {
+  const result = processNumber(globalNumber.ari);
+  globalNumber = {
+    ...globalNumber,
+    base: (Math.round(result * 1000) / 1000).toString(),
+    sub: "",
+  };
+
+  totalPointElement.innerHTML = globalNumber.base;
+};
+
+const outputAction = () => {
+  if (
+    globalNumber.phase === calPhase.start ||
+    globalNumber.phase === calPhase.middle
+  ) {
+    return;
+  }
+  totalNumber();
+  globalNumber = { ...globalNumber, phase: calPhase.start, ari: "" };
+  return;
+};
+
+const cirAction = (e) => {
+  //四則演算を連続で押したらリターン
+  if (globalNumber.phase === calPhase.middle) {
+    return;
+  }
+  //sub選択後の場合は、計算してしまう。
+  if (globalNumber.phase === calPhase.end) {
+    totalNumber();
+    globalNumber = {
+      ...globalNumber,
+      phase: calPhase.middle,
+      ari: e.dataset.key,
+    };
+    return;
+  }
+  globalNumber = {
+    ...globalNumber,
+    phase: calPhase.middle,
+    ari: e.dataset.key,
+  };
+  return;
+};
+
+const actionFirstPhase = (e) => {
+  // 最初に小数点を押したら0を頭にのせる
   if (globalNumber.base.length === 0 && e.dataset.key === ".") {
     globalNumber.base = "0";
     globalNumber.base += e.dataset.key;
+    return;
+  }
+  //base 最初に "0"を押し、次に"."を押さなかった場合、"."を押すまでエスケープする
+  if (
+    globalNumber.base.length === 1 &&
+    globalNumber.base[0] === "0" &&
+    e.dataset.key !== "."
+  ) {
+    globalNumber.base = globalNumber.base.slice(1);
+    return;
+  }
+
+  //baseを計算する
+  if (
+    e.dataset.key === "." &&
+    !isdecimalPoint(e.dataset.key, globalNumber.base)
+  ) {
+    return;
+  }
+  if (globalNumber.base.length > 10) {
+    return;
+  }
+  globalNumber.base += e.dataset.key;
+  totalPointElement.innerHTML = globalNumber.base;
+};
+
+const createNumber = (e) => {
+  // baseを入力。四則演算選択前
+  if (globalNumber.phase === calPhase.start) {
+    actionFirstPhase(e);
     return;
   }
   //sub 最初に "." を押された場合の処理
@@ -61,159 +143,62 @@ function createNumber(e) {
     globalNumber.sub += e.dataset.key;
     return;
   }
-
-  //base 最初に "0"を押し、次に"."を押さなかった場合、"."を押すまでエスケープする
-  if (globalNumber.base.length === 1) {
-    if (globalNumber.base[0] === "0" && e.dataset.key !== ".") {
-      globalNumber.base = globalNumber.base.slice(1);
-      return;
-    }
-  }
-
   //sub 最初に "0"を押し、次に"."を押さなかった場合、"."を押すまでエスケープする
-  if (globalNumber.sub.length === 1) {
-    if (globalNumber.sub[0] === "0" && e.dataset.key !== ".") {
-      globalNumber.sub = globalNumber.sub.slice(1);
-      return;
-    }
+  if (
+    globalNumber.sub.length === 1 &&
+    globalNumber.sub[0] === "0" &&
+    e.dataset.key !== "."
+  ) {
+    globalNumber.sub = globalNumber.sub.slice(1);
+    return;
   }
-  //ここまで、base,sub共通の例外処理
-
-  //baseを計算する
-  if (globalNumber.phase === calPhase.start) {
-    if (e.dataset.key === ".") {
-      if (isdecimalPoint(e.dataset.key, globalNumber.base)) {
-        globalNumber.base += e.dataset.key;
-      }
-    } else {
-      globalNumber.base += e.dataset.key;
-    }
-    if (globalNumber.base.length > 10) {
-      if (globalNumber.base[0] === "0") {
-        globalNumber.base = globalNumber.base.slice(
-          0,
-          globalNumber.base.length - 1
-        );
-        totalPointElement.innerHTML = globalNumber.base;
-      } else {
-        globalNumber.base = globalNumber.base.slice(1);
-        totalPointElement.innerHTML = globalNumber.base;
-      }
-    } else {
-      totalPointElement.innerHTML = globalNumber.base;
-    }
-  }
-
   //subを計算する
-  if (globalNumber.phase === calPhase.end) {
-    if (e.dataset.key === ".") {
-      if (isdecimalPoint(e.dataset.key, globalNumber.sub)) {
-        globalNumber.sub += e.dataset.key;
-      }
-    } else {
-      globalNumber.sub += e.dataset.key;
-    }
-    if (globalNumber.sub.length > 10) {
-      if (globalNumber.sub[0] === "0") {
-        globalNumber.sub = globalNumber.sub.slice(
-          0,
-          globalNumber.sub.length - 1
-        );
-        totalPointElement.innerHTML = globalNumber.sub;
-      } else {
-        globalNumber.sub = globalNumber.sub.slice(1);
-        totalPointElement.innerHTML = globalNumber.sub;
-      }
-    } else {
-      totalPointElement.innerHTML = globalNumber.sub;
-    }
+  if (
+    e.dataset.key === "." &&
+    !isdecimalPoint(e.dataset.key, globalNumber.sub)
+  ) {
+    return;
   }
-
-  //四則演算を選択後にsubへ移行。
+  //四則演算を選択後にsubを入力したことを確認。
   if (globalNumber.phase === calPhase.middle) {
     globalNumber.phase = calPhase.end;
   }
-}
-//少数点が複数ないか。
-function isdecimalPoint(e, num) {
-  return !num.includes(e);
-}
 
-//＝のdomにイベントを付与
-outputButtonElement.addEventListener("click", outputAction);
-//＝を押した場合の動作
-function outputAction() {
-  if (globalNumber.phase === calPhase.middle) {
+  if (globalNumber.sub.length > 10) {
     return;
   }
-  if (globalNumber.phase === calPhase.start) {
-    return;
-  }
-  if (globalNumber.phase === calPhase.end) {
-    totalNumber();
-    globalNumber.phase = calPhase.start;
-    globalNumber.ari = "";
-  }
-}
+  globalNumber.sub += e.dataset.key;
+  totalPointElement.innerHTML = globalNumber.sub;
+};
 
-//四則演算domにイベントを付与
+// dom
+//数値の要素（.）含むdom取得
+const buttonsElement = document.querySelectorAll(".js-button");
+//arithmeticのdom取得
+const actionButtonsElement = document.querySelectorAll(".js-action-button");
+//合計値表示dom
+const totalPointElement = document.querySelector(".total-point");
+//初期化するdom
+const resetButtonElement = document.querySelector(".js-reset-button");
+//出力するdom
+const outputButtonElement = document.querySelector(".js-output-button");
+
+resetButtonElement.addEventListener("click", () => {
+  resetArray();
+});
+
+buttonsElement.forEach((item) => {
+  item.addEventListener("click", () => {
+    createNumber(item);
+  });
+});
+
+outputButtonElement.addEventListener("click", () => {
+  outputAction();
+});
 
 actionButtonsElement.forEach((item) => {
-  item.addEventListener("click", function () {
+  item.addEventListener("click", () => {
     cirAction(item);
   });
 });
-//四則演算のイベント
-function cirAction(e) {
-  //四則演算を連続で押したらリターン
-  if (globalNumber.phase === calPhase.middle) {
-    return;
-  }
-  //四則演算を選択
-  if (globalNumber.phase === calPhase.start) {
-    globalNumber.phase = calPhase.middle;
-    globalNumber.ari = e.dataset.key;
-  }
-  //subを選択後、base,subを計算する。
-  if (globalNumber.phase === calPhase.end) {
-    totalNumber();
-    globalNumber.phase = calPhase.middle;
-    globalNumber.ari = e.dataset.key;
-  }
-}
-
-//実際の計算関数
-function totalNumber() {
-  const result = processNumber(globalNumber.ari);
-  globalNumber.base = Math.round(result * 1000) / 1000;
-  globalNumber.sub = "";
-  totalPointElement.innerHTML = globalNumber.base;
-}
-
-//四則演算ごとに分て計算する
-function processNumber(cir) {
-  globalNumber.base = Number(globalNumber.base);
-  globalNumber.sub = Number(globalNumber.sub);
-  if (cir === Arithmetic.sum) {
-    return globalNumber.base + globalNumber.sub;
-  }
-  if (cir === Arithmetic.sub) {
-    return globalNumber.base - globalNumber.sub;
-  }
-  if (cir === Arithmetic.mult) {
-    return adjustedNumberOfDigits(globalNumber.base * globalNumber.sub);
-  }
-  if (cir === Arithmetic.divi) {
-    return globalNumber.base / globalNumber.sub;
-  }
-}
-
-function adjustedNumberOfDigits(n) {
-  const num = 16;
-  let nString = String(n);
-  if (nString.length > num) {
-    nString = nString.slice(nString.length - num);
-    console.log(nString);
-  }
-  return Number(nString);
-}
